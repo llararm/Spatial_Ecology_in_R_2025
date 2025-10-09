c6aaedf86a63d4e9cee8cea814bd8460484681f6
Lara Maia <104166507+llararm@users.noreply.github.com>
2025-10-09 17:44:30 +0200

    Create 02_Population_density.R

---
+++ Code/02_Population_density.R
@@ -0,0 +1,88 @@
+# code for estimating population density
+# install.packages - use for download packages
+install.packages("spatstat")
+library(spatstat)
+
+
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# all the colors in R: https://r-charts.com/colors/
+
+# additional datasets
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)  #$ is used to linked different objects
+plot(bei.extra[[1]])
+
+el <- bei.extra[[1]]
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot(elevation2)
+
+# passing from points to a countinuous surface
+beidens <- density(bei) #density of organisms
+plot(beidens)
+points(bei, cex=.2)
+
+########## DAY 2
+# Plotting the maps one beside the other
+par(mfrow=c(1,2))
+plot(elevation2)
+plot(densitymap)
+
+# Exercise: make a multiframe with maps one on top of the other
+par(mfrow=c(2,1))
+plot(elevation2)
+plot(densitymap)
+
+# one frined to clear graphs
+dev.off()
+plot(elevation2)
+
+# Changin colors to maps
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+# search your browser for "colors in R" 
+# http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+
+# Exercise: change the color ramp palette using different colors
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+# Exrcise: build a multiframe and plot the densitymap with two different color ramp palettes one beside the other
+
+par(mfrow=c(1,2))
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)
+plot(densitymap, col=clg)
+
+dev.off()
