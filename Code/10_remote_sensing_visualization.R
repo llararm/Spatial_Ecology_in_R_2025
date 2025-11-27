81edafa318e4900eb8b8e5767cbc00268d3f6a60
Lara Maia <104166507+llararm@users.noreply.github.com>
2025-11-27 16:42:03 +0100

    Create 10_remote_sensing_visualization.R

---
+++ Code/10_remote_sensing_visualization.R
@@ -0,0 +1,77 @@
+# Code to visualize remote sensing data
+
+# Zenodo set: https://zenodo.org/records/15645465
+# install.packages("imageRy")
+
+library(imageRy)
+library(terra)
+
+# Listing data inside imageRy
+im.list()
+
+# Sentinel-2 bands
+# https://gisgeography.com/sentinel-2-bands-combinations/
+
+# importing the data
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(b2, col=cl)
+
+# Exercise: import b3 and plot it with the previous palette
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=cl)
+
+# Importing the red band
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=cl)
+
+# Importing the NIR band
+b8 <- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=cl)
+
+# Multiframe
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+# stack
+sentstack <- c(b2, b3, b4, b8)
+plot(sentstack, col=cl)
+
+# Plotting one layer
+dev.off()
+plot(sentstack[[1]], col=cl)
+plot(sentstack[[4]], col=cl)
+
+# Multiframe with different color palette
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+# Exercise: apply the same concept to the green band (b3)
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+# Plotting red band (b4)
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+# Plotting the NIR band (b8)
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+# RGB plotting
+# sentstack[[1]] blue
+# sentstack[[2]] green
+# sentstack[[3]] red
+# sentstack[[4]] NIR
+  
+dev.off()
+im.plotRGB(sentstack, r=3, g=2, b=1) # natural color image
+im.plotRGB(sentstack, r=4, g=3, b=2) # false color image
+im.plotRGB(sentstack, r=3, g=4, b=2) # false color image
+im.plotRGB(sentstack, r=3, g=2, b=4) # false color image
